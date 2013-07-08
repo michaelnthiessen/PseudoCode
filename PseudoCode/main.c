4942acf3d5ccb29b23b2ed0739d6d40d6b8f5e82
Michael Thiessen <electromikenetic@gmail.com>
2013-07-08 11:25:40 -0500

    Initial Commit

---
+++ PseudoCode/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  PseudoCode
+//
+//  Created by Michael Thiessen on 2013-07-08.
+//  Copyright (c) 2013 Michael Thiessen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
